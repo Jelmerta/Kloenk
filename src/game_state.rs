051e5580d1eafa156770e5fdd675cb476ffaf112
Jelmer Alphenaar <jelmer@hatsu.tech>
2023-12-29 09:30:25 +0100

    cube is moving! (WASD)

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,17 +1,57 @@
-pub struct game_state {
-    player_position_x: f64,
-    player_position_y: f64,
+pub struct GameState {
+    pub entities: Vec<Entity>,
 }
 
-impl game_state {
-    pub fn new() -> Self {
-        Self {
-            player_position_x: 0.0,
-            player_position_y: 0.0,
-        }
+pub struct Entity {
+    pub position: Position,
+}
+
+impl Entity {
+    pub fn get_position(&self) -> &Position {
+        &self.position
     }
 }
 
-// pub(crate) fn new() -> _ {
-//     todo!()
-// }
+pub struct Position {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Position {
+    pub fn get_x(&self) -> f32 {
+        self.x
+    }
+
+    pub fn get_y(&self) -> f32 {
+        self.y
+    }
+}
+
+impl GameState {
+    pub fn new() -> Self {
+        let mut entities = Vec::new();
+        let player = Entity {
+            position: Position {
+                x: 0.0,
+                y: 0.0,
+            }
+        };
+        let enemy = Entity {
+            position: Position {
+                x: 1.0,
+                y: 1.0,
+            }
+        };
+
+        entities.push(player);
+        entities.push(enemy);
+
+        Self {
+            entities: entities,
+        }
+    }
+
+    pub fn get_entities(&self) -> &Vec<Entity> { // &?
+        &self.entities
+    }
+}
