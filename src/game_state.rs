2995b79184c8740281b1defd9ca28441a3b41e89
Jelmer Alphenaar <jelmer@hatsu.tech>
2024-08-14 18:56:46 +0200

    refactoring to a more ECS-like system. All instances are grouped by (model,material) and then rendered with instancing without extra hardcoding.

--- src/game_state.rs
+++ src/game_state.rs
@@ -3,23 +3,21 @@ use std::sync::atomic::{AtomicU32, Ordering};
 pub const TOTAL_DISTANCE: f32 = 200000.; // Verify naming, probbaly not total distance
 
 pub struct GameState {
-    pub player: Entity,
     pub camera_distance: f32,
     pub camera_rotation_x_degrees: f32, // as seen on a sphere, to figure out the position of the camera. it is not
     // the direction the camera is pointed at
     pub camera_rotation_y_degrees: f32,
     pub current_entity_id: AtomicU32,
-    pub entities: Vec<Entity>,
-    pub plane: Entity,
     pub inventory_toggled: bool,
     pub inventory_position: Position, // Could be 2d. values between -1 and 1?
     pub inventory_item_count: u32,    // should be an item list
                                       //
+    pub entities: Vec<Entity>,
 }
 
 pub struct Entity {
-    pub id: u32,
-    pub entity_type: String, // Perhaps an enum at some point?
+    pub id: String,
+    pub graphics_component: GraphicsComponent,
     pub position: Position,
     pub previous_position: Position,
     pub size: Position, // HAcky reuse of position just a [f32; 3]?
@@ -30,6 +28,15 @@ impl Entity {
     pub fn get_position(&self) -> &Position {
         &self.position
     }
+
+    pub fn update_position(&mut self, position: Position) {
+        self.position = position;
+    }
+}
+
+pub struct GraphicsComponent {
+    pub model_id: String,
+    pub material_id: String,
 }
 
 #[derive(Clone, Copy)]
@@ -55,10 +62,13 @@ impl Position {
 
 impl GameState {
     pub fn new() -> Self {
-        let mut entities = Vec::new();
         let player = Entity {
-            id: u32::MAX, // hacky, just should not be used. player probably does not need id (yet)
-            entity_type: "character".to_string(),
+            // id: u32::MAX,
+            id: "player".to_string(), // hacky, just should not be used. player probably does not need id (yet)
+            graphics_component: GraphicsComponent {
+                model_id: "character".to_string(),
+                material_id: "character".to_string(),
+            },
             position: Position {
                 x: 0.0,
                 y: 0.0,
@@ -81,8 +91,12 @@ impl GameState {
 
         let current_entity_id: AtomicU32 = AtomicU32::new(0);
         let sword = Entity {
-            id: current_entity_id.fetch_add(1, Ordering::SeqCst),
-            entity_type: "sword".to_string(), 
+            // id: current_entity_id.fetch_add(1, Ordering::SeqCst),
+            id: "sword".to_string(),
+            graphics_component: GraphicsComponent {
+                model_id: "sword".to_string(),
+                material_id: "sword".to_string(),
+            },
             position: Position {
                 x: 1.1,
                 y: 1.1,
@@ -101,11 +115,14 @@ impl GameState {
             hitbox: 0.51,
         };
 
-        entities.push(sword);
 
         let plane = Entity {
-            id: u32::MAX - 1,
-            entity_type: "grass".to_string(),
+            // id: u32::MAX - 1,
+            id: "plane".to_string(),
+            graphics_component: GraphicsComponent {
+                model_id: "grass".to_string(),
+                material_id: "grass".to_string(),
+            },
             position: Position {
                 x: 0.0,
                 y: 0.0,
@@ -124,14 +141,16 @@ impl GameState {
             hitbox: 0.0,
         };
 
+        let mut entities = Vec::new();
+        entities.push(player);
+        entities.push(sword);
+        entities.push(plane);
+
         Self {
-            player,
             camera_distance,
             camera_rotation_x_degrees: 225.0,
             camera_rotation_y_degrees: 315.0,
             current_entity_id,
-            entities,
-            plane,
             inventory_toggled: false,
             inventory_position: Position {
                 x: 1.33,
@@ -139,6 +158,7 @@ impl GameState {
                 z: 0.0,
             },
             inventory_item_count: 1,
+            entities,
         }
     }
 
@@ -147,22 +167,41 @@ impl GameState {
         &self.entities
     }
 
-    pub fn remove_entity_from_world(&mut self, entity_to_remove: u32) {
+    // pub fn remove_entity_from_world(&mut self, entity_to_remove: u32) {
+    //     self.entities.retain(|entity| entity.id != entity_to_remove);
+    // }
+
+    pub fn remove_entity_from_world(&mut self, entity_to_remove: String) {
         self.entities.retain(|entity| entity.id != entity_to_remove);
     }
-
+    
     pub(crate) fn add_item_to_inventory(&mut self) {
         self.inventory_item_count = self.inventory_item_count + 1;
     }
 
-    pub fn get_entity(&self, id: u32) -> Option<&Entity> {
+    // pub fn get_entity(&self, id: u32) -> Option<&Entity> {
+    //     self.entities.iter().find(|entity| entity.id == id)
+    // }
+
+    pub fn get_entity(&self, id: String) -> Option<&Entity> {
         self.entities.iter().find(|entity| entity.id == id)
     }
 
-    pub fn new_entity(&self, placement_position: Position, entity_type: String) -> Entity {
+    pub fn get_player_const(&self) -> &Entity {
+        self.entities.iter().find(|entity| entity.id == "player").unwrap()
+    }
+    pub fn get_player(&mut self) -> &mut Entity {
+        self.entities.iter_mut().find(|entity| entity.id == "player").unwrap()
+    }
+    
+    pub fn new_entity(&self, placement_position: Position) -> Entity {
         return Entity {
-            id: self.current_entity_id.fetch_add(1, Ordering::SeqCst),
-            entity_type,
+            // id,
+            id: self.current_entity_id.fetch_add(1, Ordering::SeqCst).to_string(),
+            graphics_component: GraphicsComponent {
+                model_id: "sword".to_string(),
+                material_id: "sword".to_string(),
+            },
             position: placement_position,
             previous_position: placement_position,
             size: Position {
