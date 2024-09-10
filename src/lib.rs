dda762a802afd0c0794565fa1e3ac6b35ebf43c1
Jelmer Alphenaar <jelmer@hatsu.tech>
2024-09-10 10:16:27 +0200

    transition to webgpu 22 complete? Man webgpu seems a lot darker than opengl. need some lighting in this world

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,20 @@
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+use winit::{event::*, event_loop::EventLoop, keyboard::PhysicalKey};
 
 use crate::game_system::GameSystem;
 
-mod texture;
-mod input;
-mod game_state;
-mod render;
-mod game_system;
 mod camera;
-mod model;
-mod resources;
 mod components;
+mod game_state;
+mod game_system;
 mod gui;
+mod input;
+mod model;
+mod render;
+mod resources;
+mod texture;
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
@@ -31,102 +27,120 @@ pub async fn run() {
         }
     }
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let event_loop = EventLoop::new().unwrap();
+    let window = winit::window::WindowBuilder::new()
+        .with_title("Kloenk!")
+        .build(&event_loop)
+        .unwrap();
 
     #[cfg(target_arch = "wasm32")]
     {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        use winit::dpi::PhysicalSize;
-        window.set_inner_size(PhysicalSize::new(800, 600));
+   
 
         use winit::platform::web::WindowExtWebSys;
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| {
                 let dst = doc.get_element_by_id("kloenk-wasm")?;
-                let canvas = web_sys::Element::from(window.canvas());
+                let canvas = web_sys::Element::from(window.canvas()?);
                 dst.append_child(&canvas).ok()?;
                 Some(())
             })
             .expect("Couldn't append canvas to document body.");
+        // TODO Sizing after setting canvas otherwise not set?
+     // Winit prevents sizing with CSS, so we have to set
+        // the size manually when on web.
+        use winit::dpi::PhysicalSize;
+        let _ = window.request_inner_size(PhysicalSize::new(800, 600));
     }
 
     // State::new uses async code, so we're going to wait for it to finish
-    let mut state = render::State::new(window).await;
+    let mut state = render::State::new(&window).await;
 
     let mut game_state = game_state::GameState::new();
     let mut ui_state = gui::UIState::new();
     let mut input_handler = input::Input::new();
 
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == state.window().id() => {
-                if !state.input(event) {
-                    // UPDATED!
-                    match event {
-                        WindowEvent::CloseRequested
-                        | WindowEvent::KeyboardInput {
-                            input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                            ..
-                        } => *control_flow = ControlFlow::Exit,
-                        WindowEvent::KeyboardInput {
-                            input: KeyboardInput {
-                                state: element_state,
-                                virtual_keycode: Some(key),
-                                ..
-                            },
-                            ..
-                        } => {
-                            input_handler.update(key, element_state);
-                        }
-                        WindowEvent::MouseInput { state, button, .. } => {
-                            input_handler.process_mouse_button(button, state);
-                        }
-                        WindowEvent::MouseWheel { delta, .. } => {
-                            input_handler.process_scroll(delta);
-                            true;
-                        }
-                        WindowEvent::Resized(physical_size) => {
-                            state.resize(*physical_size);
-                        }
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            // new_inner_size is &&mut so w have to dereference it twice
-                            state.resize(**new_inner_size);
-                        }
-                        _ => {}
-                    }
-                }
-            }
-            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-                GameSystem::update(&mut game_state, &mut ui_state, &mut input_handler);
-                match state.render(&game_state, &ui_state) {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        state.resize(state.size)
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+    // TODO Does this help with resizing issue where it keeps rendering larger or something?
+    // no it doesn't seem to do much... probably can remove
+    let mut surface_configured = false;
 
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+    event_loop
+        .run(move |event, control_flow| {
+            match event {
+                Event::WindowEvent {
+                    ref event,
+                    window_id,
+                } if window_id == state.window().id() => {
+                    if !state.input(event) {
+                        match event {
+                            #[cfg(not(target_arch = "wasm32"))]
+                            WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                event:
+                                    KeyEvent {
+                                        physical_key:
+                                            PhysicalKey::Code(winit::keyboard::KeyCode::Escape),
+                                        state: ElementState::Pressed,
+                                        ..
+                                    },
+                                ..
+                            } => control_flow.exit(),
+                            WindowEvent::KeyboardInput {
+                                event:
+                                    KeyEvent {
+                                        physical_key: PhysicalKey::Code(key),
+                                        state,
+                                        ..
+                                    },
+                                ..
+                            } => {
+                                input_handler.update(key, state);
+                            }
+                            WindowEvent::MouseInput { state, button, .. } => {
+                                input_handler.process_mouse_button(button, state);
+                            }
+                            WindowEvent::MouseWheel { delta, .. } => {
+                                input_handler.process_scroll(delta);
+                                true;
+                            }
+                            WindowEvent::Resized(physical_size) => {
+                                surface_configured = true;
+                                state.resize(*physical_size);
+                            }
+                            WindowEvent::RedrawRequested => {
+                                state.window().request_redraw();
+                                if !surface_configured {
+                                    return;
+                                }
+                                GameSystem::update(
+                                    &mut game_state,
+                                    &mut ui_state,
+                                    &mut input_handler,
+                                );
+                                match state.render(&game_state, &ui_state) {
+                                    Ok(_) => {}
+                                    // Reconfigure the surface if it's lost or outdated
+                                    Err(
+                                        wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated,
+                                    ) => state.resize(state.size),
+                                    // The system is out of memory, we should probably quit
+                                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                                        control_flow.exit();
+                                    }
+
+                                    Err(wgpu::SurfaceError::Timeout) => {
+                                        log::warn!("Surface timeout")
+                                    }
+                                }
+                            }
+
+                            _ => {}
+                        }
+                    }
                 }
+                _ => {}
             }
-            Event::RedrawEventsCleared => {
-                // RedrawRequested will only trigger once, unless we manually
-                // request it.
-                state.window().request_redraw();
-            }
-            _ => {}
-        }
-    });
+        })
+        .unwrap();
 }
