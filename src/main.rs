2e0d10054fe6ab2363049a04ff8d48f4c54ef41e
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2024-09-22 23:10:04 +0200

    separate wasm main such that we do not build second main

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,12 @@ use kloenk::Application;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 fn main() {
+    run();
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
+fn run () {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
