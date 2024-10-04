b6b16b679962efb04005d40be85fe85620e44c67
Jelmer Alphenaar <jelmer@hatsu.tech>
2024-10-05 00:18:41 +0200

    rebuild caches

--- src/game_state.rs
+++ src/game_state.rs
@@ -84,7 +84,7 @@ impl GameState {
         graphics_2d_components.insert(shield.clone(), shield_graphics_inventory);
 
         let shield_position = Position {
-            x: -2.7,
+            x: -2.6,
             y: -2.5,
             z: 0.0,
         };
