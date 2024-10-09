7fba4ed0177c6bab6464543ac4ede4f173000597
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2024-10-09 12:28:31 +0200

    pedantic clippy changes

--- src/resources.rs
+++ src/resources.rs
@@ -161,6 +161,7 @@ fn format_url(file_name: &str) -> reqwest::Url {
     base.join("resources/").unwrap().join(file_name).unwrap()
 }
 
+#[allow(clippy::unused_async)]
 pub async fn load_binary(file_name: &str) -> anyhow::Result<Vec<u8>> {
     cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
