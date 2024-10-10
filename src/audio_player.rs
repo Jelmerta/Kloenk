05a48c09bd1a84cd4e352f4d96077ccc058e7847
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2024-10-10 08:03:06 +0200

    check web sound

--- src/audio_player.rs
+++ src/audio_player.rs
@@ -1,16 +1,27 @@
 use crate::resources::load_binary;
+use rodio::{OutputStream, OutputStreamHandle, Sink, Source};
 use std::io::Cursor;
+// use wasm_thread as thread;
 
 pub struct AudioPlayer {
     pub tmp: String,
+    stream: OutputStream,
+    handle: OutputStreamHandle,
+    sink: Sink,
     // sounds: HashMap<String, Sound>,
 }
 
 impl AudioPlayer {
     // pub async fn new() -> Self {
     pub fn new() -> Self {
+        let (stream, handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&handle).unwrap();
+
         AudioPlayer {
             // sounds: HashMap::new(),
+            stream,
+            sink,
+            handle,
             tmp: String::new(),
         }
 
@@ -35,17 +46,23 @@ impl AudioPlayer {
     // }
 
     //, sound: &str
-    pub fn play_audio() {
-        std::thread::spawn(move || {
-            let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
-            let sink = rodio::Sink::try_new(&handle).unwrap();
+    pub fn play_audio(&self) {
+        // std::thread::spawn(move || {
+        // let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+        // let sink = rodio::Sink::try_new(&handle).unwrap();
 
-            let sound_bytes = pollster::block_on(load_binary("bonk.mp3")).unwrap();
-            let audio_cursor = Cursor::new(sound_bytes);
-            sink.append(rodio::Decoder::new(audio_cursor).unwrap());
+        let sound_bytes = pollster::block_on(load_binary("bonk.wav")).unwrap();
+        let audio_cursor = Cursor::new(sound_bytes);
+        let source = rodio::Decoder::new(audio_cursor).unwrap();
 
-            sink.sleep_until_end();
-        });
+        self.handle.play_raw(source.convert_samples()).unwrap();
+        // self.sink.append(source);
+
+        // self.sink.sleep_until_end();
+        // self.sink.;
+        // thread::sleep(Duration::from_millis(3000));
+        // self.sink.detach();
+        // });
     }
 }
 
