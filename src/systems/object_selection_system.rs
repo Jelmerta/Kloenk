a71fc2841a1971cde170e4acb6405e094b3619d7
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2024-12-05 08:46:36 +0100

    world item menu

--- src/systems/object_selection_system.rs
+++ src/systems/object_selection_system.rs
@@ -1,5 +1,5 @@
 use crate::state::components::Entity;
-use crate::state::frame_state::FrameState;
+use crate::state::frame_state::{ActionEffect, FrameState};
 use crate::state::game_state::GameState;
 use crate::state::input::Input;
 use crate::state::ui_state::MenuState::Closed;
@@ -21,42 +21,73 @@ impl ObjectSelectionSystem {
         if Self::should_open_menu(input, frame_state, selected_objects) {
             ui_state.menu_state = MenuState::World {
                 mouse_position: input.mouse_position_ui,
-                item: selected_objects.get(0).unwrap().clone(),
+                item: selected_objects.first().unwrap().clone(),
             };
         }
 
-        match &ui_state.menu_state {
-            MenuState::World {
-                mouse_position,
-                item,
-            } => {
-                let object_selection_rect = Rect::new(
-                    Point2::new(mouse_position.x - 0.05, mouse_position.y - 0.05),
-                    Point2::new(mouse_position.x + 0.15, mouse_position.y + 0.05),
-                );
+        let mut new_menu_state = ui_state.menu_state.clone();
+        if let MenuState::World {
+            mouse_position,
+            item,
+        } = &ui_state.menu_state
+        {
+            let pickup_menu_rect = Rect::new(
+                Point2::new(mouse_position.x - 0.05, mouse_position.y - 0.02),
+                Point2::new(mouse_position.x + 0.08, mouse_position.y + 0.04),
+            );
 
-                // TODO Specifically show text to pickup item
-                match frame_state.gui.image_button(
-                    200,
-                    object_selection_rect,
-                    "sword_inventory".to_string(),
-                    input,
-                ) {
-                    UserAction::LeftClick => {
-                        if frame_state.handled_left_click {
-                            return;
-                        }
-                        ItemPickupSystem::item_pickup(game_state, frame_state, item.clone());
-                        ui_state.menu_state = Closed;
-                        frame_state.handled_left_click = true;
+            let mut text_color = [0.8, 0.8, 0.8];
+            match frame_state.gui.color_button(200, pickup_menu_rect, input) {
+                UserAction::LeftClick => {
+                    if frame_state.handled_left_click {
+                        return;
                     }
-                    _ => (),
+                    ItemPickupSystem::item_pickup(game_state, frame_state, item.clone());
+                    new_menu_state = Closed;
+                    frame_state.handled_left_click = true;
                 }
-
-                frame_state.handled_right_click = true;
+                UserAction::None => {}
+                UserAction::Hover => text_color = [0.8, 0.8, 0.0],
+                UserAction::RightClick => {}
             }
-            _ => (),
+            frame_state.gui.text(
+                300,
+                pickup_menu_rect,
+                "Pick up item".to_string(),
+                text_color,
+            );
+
+            let examine_menu_rect = Rect::new(
+                Point2::new(mouse_position.x - 0.05, mouse_position.y + 0.04),
+                Point2::new(mouse_position.x + 0.08, mouse_position.y + 0.10),
+            );
+            let mut text_color = [0.8, 0.8, 0.8];
+            match frame_state.gui.color_button(200, examine_menu_rect, input) {
+                UserAction::LeftClick => {
+                    if frame_state.handled_left_click {
+                        return;
+                    }
+                    let examine_text = game_state.description_components.get(item).unwrap();
+                    frame_state.action_effects.push(ActionEffect::Examine {
+                        text: examine_text.text.clone(),
+                    });
+                    new_menu_state = Closed;
+                    frame_state.handled_left_click = true;
+                }
+                UserAction::None => {}
+                UserAction::Hover => text_color = [0.8, 0.8, 0.0],
+                UserAction::RightClick => {}
+            }
+            frame_state.gui.text(
+                300,
+                examine_menu_rect,
+                "Examine item".to_string(),
+                text_color,
+            );
         }
+        ui_state.menu_state = new_menu_state;
+
+        frame_state.handled_right_click = true;
     }
 
     fn should_open_menu(
