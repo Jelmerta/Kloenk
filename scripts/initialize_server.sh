4b45e0cb4414bebd234c12827bc6f58ec70c54e2
Jelmer Alphenaar <6730555+Jelmerta@users.noreply.github.com>
2025-06-08 11:30:04 +0200

    serving acme-challenge

--- scripts/initialize_server.sh
+++ scripts/initialize_server.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Currently these steps are executed manually because some interaction is required, next time we setup a server maybe try to make this script executable in one go
+
+sudo apt update
+
 # First: Install Docker! https://docs.docker.com/engine/install/debian/#install-using-the-repository
 # Manual script to run on the server to initialise a server such that we can externally give a sign to retrieve and deploy the container with the game.
 # This only needs to be run once for each server.
@@ -16,15 +20,24 @@ cat .ssh/id_ed25519.pub > .ssh/authorized_keys
 
 # Perform the necessary GitHub steps, add 3 secrets and a deploy key
 
-# Make sure firewall allows access on 80/443 for http/https
+# Make sure firewall allows access on 80/443 for http/https and 22 for ssh
 apt-get install ufw
 ufw enable
 ufw allow ssh
 ufw allow http
 ufw allow https
 
+# Connecting to the server can be done as such after setting up ssh:
+#ssh -i ~/.ssh/id_ed25519_kloenk root@xxx.xxx.xxx.xxx
+
 # Setup certbot to get certs from lets encrypt for SSL/HTTPS:
 # https://certbot.eff.org/instructions?ws=nginx&os=pip
+sudo apt install python3 python3-dev python3-venv libaugeas-dev gcc
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
 # To set up a certbot, we create a new directory to add the certs to, as certbot keeps symbolic links to a different location and we want the full file to be mounted as a volume when starting the docker container
 sudo mkdir -p /etc/ssl-kloenk
 sudo cp -L /etc/letsencrypt/live/hatsu.tech/cert.pem /etc/ssl-kloenk/cert.pem
@@ -34,5 +47,13 @@ sudo cp -L /etc/letsencrypt/live/hatsu.tech/privkey.pem /etc/ssl-kloenk/privkey.
 sudo mkdir -p /var/www/certbot-acme
 sudo chmod -R 755 /var/www/certbot-acme
 
-# Connecting to the server can be done as such:
-ssh -i ~/.ssh/id_ed25519_kloenk root@xxx.xxx.xxx.xxx
+# First time we need to setup ssl as such:
+sudo certbot certonly --webroot \
+  -w /var/www/certbot-acme \
+  --domain hatsu.tech \
+  --email myemailaddress@hatsu.tech \
+  --non-interactive \
+  --agree-tos
+
+# To renew the certificates automatically, we setup a cron job
+echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && sudo certbot renew -q --webroot -w /var/www/certbot-acme" | sudo tee -a /etc/crontab > /dev/null
